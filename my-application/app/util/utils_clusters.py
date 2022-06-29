16b16f9ac73511d32752bdb0f3f2e77d1a5a5f6d
https://gitlab.epfl.ch/schaerf <ludovica.schaerf@epfl.ch>
2022-06-29 16:51:39 +0200

    fixed bug

--- my-application/app/util/utils_clusters.py
+++ my-application/app/util/utils_clusters.py
@@ -13,7 +13,7 @@ from sklearn.mixture import BayesianGaussianMixture
 
 
 
-def show_results_button(cluster_df, data, map_file):
+def show_results_button(cluster_df, data, map_file, data_dir='../data/'):
     """_summary_
 
     Args:
@@ -51,7 +51,7 @@ def show_results_button(cluster_df, data, map_file):
     else:
         cluster = cluster_df.groupby('cluster').first().sample(1).index.values
       
-    INFO = images_in_clusters(cluster_df[cluster_df['cluster'].isin(cluster)], data, map_file=map_file)
+    INFO = images_in_clusters(cluster_df[cluster_df['cluster'].isin(cluster)], data, map_file=map_file, data_dir=data_dir)
     return INFO, ','.join([str(cl) for cl in cluster])
 
 
